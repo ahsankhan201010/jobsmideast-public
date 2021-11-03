9f21f96501af268568a4f16deac56237072a8020
MAK <ahsankhan201010@gmail.com>
2021-11-03 10:44:50 +0500

    initial boiler-plate code

---
+++ components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import style from "./Header.module.scss"
+
+const Header = () => {
+    return (
+        <div className={style.header}>
+            <h1>Header</h1>
+        </div>
+    )
+}
+
+export default Header
